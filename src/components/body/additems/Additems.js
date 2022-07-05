52872011c8132e8d04ee2347981adb1a5d82f529
Anteneh Solomon <antenehcs@gmail.com>
2022-07-05 18:49:34 +0300

    adding items

--- src/components/body/additems/Additems.js
+++ src/components/body/additems/Additems.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import '../../styles/style.css'
 
 const Additems = () => {
   return (
-    <div>
-      
+    <div className='additemssec'>
+      <div className='additemsmain'>
+            <form>
+              <label className='conlabel'>Name</label><br/>
+              <input className='coninput'/><br/>
+              <label className='conlabel'>Gender-For</label><br/>
+              <input className='coninput'/><br/>
+              <label className='conlabel'>Age-For</label><br/>
+              <input className='coninput'/><br/>
+              <label className='conlabel'>Photos</label><br/>
+              <input className='coninput'/><br/>
+              <label className='conlabel'>Price</label><br/>
+              <input className='coninput'/><br/>
+              <label className='conlabel'>Description</label><br/>
+              <input className='coninput'/><br/>
+              <input className='submail' type="submit" value="Add"></input>
+            </form>
+          </div>
     </div>
   )
 }
--- src/components/body/viewbooked/Viewbooked.js
+++ src/components/body/viewbooked/Viewbooked.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Viewbooked = () => {
   return (
     <div>
-      
+      <h1>View booked</h1>
     </div>
   )
 }
