79cebad03604c6b47fd6bbcdd2142c8e54f7c999
Anteneh Solomon <antenehcs@gmail.com>
2022-07-10 15:04:30 +0300

    update state

--- src/components/cards/ClotheBox.js
+++ src/components/cards/ClotheBox.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom';
 import '../styles/style.css'
 import photo from '../Images/imagetwo.jpg'
 
-const ClotheBox = () => {
+const ClotheBox = (props) => {
+  const [name,setname] = useState(props.name);
+  const [price,setprice] = useState(props.price);
+  const [description,setdescription] = useState(props.description);
+  const [photos,setphotos] = useState(props.photo);
+  const [id,setid] = useState(props.id);
   return (
     <div className='mainbox'>
       <div className='container'>
@@ -11,9 +16,9 @@ const ClotheBox = () => {
         <img src={photo} alt='' className="pic"/>
         </div>
         <div className='intro'>
-          <h5 className='details'>Name</h5>
+          <h5 className='details'>{props.name}</h5>
           <div className='but'>
-            <Link  to="/detailed" className='detailsbut'>
+            <Link  to="/detailed"  state={{fname:name,fphoto:photos,fprice:price,fdescription:description, fid:id }} className='detailsbut'>
               <li className='linkdetails'><h5 className='detailsbut'>View</h5></li>
             </Link>
           </div>
