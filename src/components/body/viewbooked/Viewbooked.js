2a68954adde0f44926f485fa8f7105d3c0c07480
Anteneh Solomon <antenehcs@gmail.com>
2022-07-10 17:21:06 +0300

    Booking List

--- src/components/body/viewbooked/Viewbooked.js
+++ src/components/body/viewbooked/Viewbooked.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import axios from "axios";
 import BookedBox from '../../cards/BookedBox'
 
 const Viewbooked = () => {
+  const baseURL = "http://localhost/piyankiya/api/post/readbooking.php";
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
   return (
     <div className='viewbookedmain'>
-      <BookedBox/>
-      <BookedBox/>
-      <BookedBox/>
-      <BookedBox/>
-      <BookedBox/>
-      <BookedBox/>
+      {post.data.map((e) => (
+              <BookedBox name={e.name} id={e.cid} email={e.email} phone={e.phone}/>
+            ))}
     </div>
   )
 }
--- src/components/cards/BookedBox.js
+++ src/components/cards/BookedBox.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/style.css'
 
-const BookedBox = () => {
+const BookedBox = (props) => {
   return (
     <div>
         <div className='bookedboxmain'>
-            <h4>Client Name: </h4>
-            <h4>Client Phone: </h4>
-            <h4>E-mail: </h4>
-            <h4>Cloth_ID: </h4>
+          <div className='bookedboxinner'>
+            <h4>Client Name: {props.name} </h4>
+            <h4>Client Phone:{props.phone}</h4>
+            <h4>E-mail: {props.email}</h4>
+            <h4>Cloth_ID: {props.id}</h4>
+          </div>
         </div>
     </div>
   )
