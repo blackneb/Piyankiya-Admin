9d3b7b08b71893b51c7b7e8a9ac62a85943c07a1
Anteneh Solomon <antenehcs@gmail.com>
2022-08-27 16:51:14 +0300

    occasion redux

--- src/components/body/occasion/Occasion.js
+++ src/components/body/occasion/Occasion.js
@@ -3,17 +3,12 @@ import Break from '../break/Break'
 import ClotheBox from '../../cards/ClotheBox'
 import '../../styles/style.css'
 import axios from "axios";
+import {useSelector} from 'react-redux';
+import { ActionTypes } from '../../../Redux/Constants/ActionTypes';
 
 const Occasion = () => {
-  const baseURL = "http://blackneb.com/piyankiya/api/post/read_byt.php?types=occasion";
-  const [post, setPost] = React.useState("");
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  if (!post) return null;
+  const clothes = useSelector(state => state.clothes.clothes);
+  let occasionclothe = clothes.filter(clothe => clothe.types === 'occasion');
   return (
     <div>
     <Break/>
@@ -21,15 +16,15 @@ const Occasion = () => {
     <div className='homewo'>
       <div className='homewoboxes'>
       {(()=>{
-                    if(post.message==="no posts found"){
+                    if(occasionclothe==="no posts found"){
                         return(
                             <h1>we will add contents soon</h1>
                         )
                     }
                     else{
                         return(
-                          [...post.data].reverse().map((e) => (
-                            <ClotheBox key={e.id} name={e.name} id={e.id} price={e.price} description={e.description} photo={e.photos} age={e.afor} gender={e.gfor} types={e.types}/>          
+                          [...occasionclothe].reverse().map((e) => (
+                            <ClotheBox key={e.id} name={e.name} id={e.id} price={e.price} description={e.description} photo={ActionTypes.PHOTOURL + e.photos} age={e.afor} gender={e.gfor} types={e.types}/>          
                             ))
                         )
                     }
