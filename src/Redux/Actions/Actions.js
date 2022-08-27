4336db0685a6c5103093bcf5c8b5e7809b0d4222
Anteneh Solomon <antenehcs@gmail.com>
2022-08-27 11:27:52 +0300

    actions and constants

---
+++ src/Redux/Actions/Actions.js
@@ -0,0 +1,29 @@
+import { ActionTypes } from "../Constants/ActionTypes";
+
+export const set_men_clothes = (clothes) => {
+    return{
+        type:ActionTypes.SET_MEN_CLOTHE,
+        payload:clothes,
+    };
+};
+
+export const set_women_clothes = (clothes) => {
+    return{
+        type:ActionTypes.SET_WOMEN_CLOTHE,
+        payload:clothes,
+    };
+};
+
+export const set_kids_clothes = (clothes) => {
+    return{
+        type:ActionTypes.SET_KIDS_CLOTHE,
+        payload:clothes,
+    };
+};
+
+export const set_occasion_clothes = (clothes) => {
+    return{
+        type:ActionTypes.SET_OCCASION_CLOTHE,
+        payload:clothes,
+    };
+};
